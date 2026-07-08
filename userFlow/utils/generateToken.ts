d1e32e83aa01d645b97be9243e3e28054b1e3f5a
Tanay Thatte <tanaythatte17@gmail.com>
2026-07-08 22:00:23 +0530

    Removed token log

--- userFlow/utils/generateToken.ts
+++ userFlow/utils/generateToken.ts
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (payload: object, res: any) => {
     const token = jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: "5d" });
-    console.log("Generated JWT token:", token);
     const isProduction = process.env.NODE_ENV === 'production';
     res.cookie("jwt", token, {
         maxAge: 5 * 24 * 60 * 60 * 1000,
